30c5f3ea9802bafe4999eaee3aeb9cd57687bde1
Clayder <peterclayder@gmail.com>
2020-03-12 20:44:09 -0300

    Criei a view que lista as questions

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from polls.models import Question
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Olá")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
